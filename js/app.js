996ca81557ed7f64c55ae7c4e33df822b1ce33ca
Danne_sch <35847188+Dannesch@users.noreply.github.com>
2022-03-15 15:27:59 +0100

    Testing new navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 particlesJS.load('particles-js', 'js/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
+
+
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
