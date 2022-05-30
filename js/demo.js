84fe3f423c06bcabab78d7319a72b4df9957e2f0
Rasandemusen <98536015+Rasandemusen@users.noreply.github.com>
2022-05-30 09:06:51 +0200

    fixat till de olika subsidorna
    bytt bilder samt fixat länkar mellan sidorna i navbaren.

--- js/demo.js
+++ js/demo.js
@@ -6,11 +6,3 @@ $(function() {
   });
   
 });
-
-$('.navTrigger').click(function () {
-  $(this).toggleClass('active');
-  console.log("Clicked menu");
-  $("#mainListDiv").toggleClass("show_list");
-  $("#mainListDiv").fadeIn();
-
-});
